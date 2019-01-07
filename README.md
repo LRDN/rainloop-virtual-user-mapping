7e31f40bbef074f04fd261b1430e02e8fefa640f
LRDN <git@lrdn.net>
2019-01-07 19:05:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Virtual User Mapping
+====================
+
+[Rainloop Webmail](https://github.com/rainloop/rainloop-webmail) plugin which automatically maps email addresses to the corresponding usernames defined in an alias table, primarily used by the mail server [Postfix](http://www.postfix.org).
+
+Installation
+------------
+
+Clone this repository into your plugins directory.
+
+```
+git clone https://github.com/lrdn/rainloop-virtual-user-mapping.git virtual-user-mapping
+```
+
+Configuration
+-------------
+
+1. Open the webmail admin panel
+2. Go to `Plugins` and enable `virtual-user-mapping`
+
+In the plugin configuration you have the option to set a custom path to the mapping table.
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+	class VirtualUserMappingPlugin extends \RainLoop\Plugins\AbstractPlugin
+	{
+		public function Init()
+		{
+			$this->addHook('filter.login-credentials', 'FilterLoginСredentials');
+		}
+
+		public function FilterLoginСredentials(&$sEmail, &$sLogin, &$sPassword)
+		{
+			$sMapping = \trim($this->Config()->Get('plugin', 'mapping_table', ''));
+
+			if (is_file($sMapping) && is_readable($sMapping)) {
+				$sMapping = file_get_contents($sMapping);
+
+				if (empty($sMapping) === false) {
+					$aLines = \preg_split('/\v+/', $sMapping);
+
+					foreach ($aLines as $sLine) {
+						$aData = \preg_split('/\h+/', trim($sLine));
+
+						if (isset($aData[0], $aData[1]) && $aData[0] === $sEmail && preg_match('/^[0-9a-z_\.\-]+$/i', $aData[1])) {
+							$sLogin = $aData[1];
+						}
+					}
+				}
+			}
+		}
+
+		public function configMapping()
+		{
+			return array(
+				\RainLoop\Plugins\Property::NewInstance('mapping_table')->SetLabel('Mapping table')
+					->SetType(\RainLoop\Enumerations\PluginPropertyType::STRING)
+					->SetDescription('Absolute path to the mapping table')
+					->SetDefaultValue('/etc/postfix/virtual/addresses')
+			);
+		}
+	}
